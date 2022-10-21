9ae56acc5d159ac69458cc65b8844e7bc4f07b33
XuedongZhou <zhouxuedong0877@gmail.com>
2022-10-21 10:43:01 +0800

    fix: update docs baseUrl

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/se-ui-vue/',
   title: 'se-ui-vue',
   description: 'se-ui vue components library',
   themeConfig: {
